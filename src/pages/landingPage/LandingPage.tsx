f607ec5b288e5394aaa16576dc8189f9c2e380a3
Sachin <sachinbhattarai55@gmail.com>
2023-06-14 22:03:02 +0545

    weather API added and bg image added

--- src/pages/landingPage/LandingPage.tsx
+++ src/pages/landingPage/LandingPage.tsx
@@ -1,20 +1,58 @@
 import React from 'react';
-import { ContainerStyled, LandingPageStyled, MenuStyled } from './styles';
+import {
+  ContainerStyled,
+  InfoTextStyled,
+  LandingPageStyled,
+  MenuStyled
+} from './styles';
 import { useCustomTheme } from '../../context/themeContext';
 import Header from './Header';
 import Weather from './Weather';
-
+import { Text } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import {
+  FcCommandLine,
+  FcContacts,
+  FcReadingEbook,
+  FcServices,
+  FcSurvey
+} from 'react-icons/fc';
 export interface ILandingPageProps {}
 
 const LandingPage: React.FC<ILandingPageProps> = () => {
-  const { colors, changeColors } = useCustomTheme();
+  const { colors } = useCustomTheme();
 
   return (
     <LandingPageStyled isLandedOnEvenTime={false} colors={colors}>
       <Header />
-      <ContainerStyled>
+      <ContainerStyled colors={colors}>
+        <InfoTextStyled colors={colors}>
+          <Text className="name">Sachin Bhattarai</Text>
+          <Text className="sub-text"> Web Developer </Text>
+        </InfoTextStyled>
         <Weather />
-        <MenuStyled>Menus</MenuStyled>
+        <MenuStyled colors={colors}>
+          <Link to="/">
+            <FcSurvey className="icon" />
+            CV
+          </Link>
+          <Link to="/">
+            <FcReadingEbook className="icon" />
+            Blogs
+          </Link>
+          <Link to="/">
+            <FcCommandLine className="icon" />
+            Projects
+          </Link>
+          <Link to="/">
+            <FcContacts className="icon" />
+            Contact
+          </Link>
+          <Link to="/">
+            <FcServices className="icon" />
+            API's
+          </Link>
+        </MenuStyled>
       </ContainerStyled>
     </LandingPageStyled>
   );
